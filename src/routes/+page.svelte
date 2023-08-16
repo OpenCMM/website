eb04442eacaffb1ab58e75999c4c96f27d846b89
Yuichiro Aoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-08-16 22:13:12 +0900

    Refactor (#1)
    * add carbon
    
    * locale
    
    * add demo
    
    * blank

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,90 @@
+<script>
+	import { _ } from 'svelte-i18n';
+	import { Content, Grid, Row, OrderedList, ListItem } from 'carbon-components-svelte';
+	import demo from '$lib/images/opencmm-v0.1.0.jpg';
+</script>
+
 <svelte:head>
 	<title>OpenCMM</title>
 	<meta name="description" content="OpenCMM" />
 </svelte:head>
 
 <section>
-	<h1>
-		OpenCMM
-	</h1>
-
-	<p>
-		We are building an image-based CMM (Coordinate Measuring Machine) that is open source and affordable.
-	</p>
-
-	<p>
-		We focus on measuring the dimensions of the object inside CNC machines.
-	</p>
-
-	<p>
-		This will save centering and measuring time.
-	</p>
-
-
-	<p>
-		We will appreciate your feedbacks and if you have any questions, please contact us via <a href="https://twitter.com/OpenCMM">Twitter</a>.
-	</p>
+	<Content>
+		<Grid>
+			<Row>
+				<p>
+					{$_('home.description')}
+				</p>
+			</Row>
+			<Row>
+				<h2>{$_('home.why.title')}</h2>
+				<p>
+					{$_('home.why.reason')}
+				</p>
+				<p>
+					{$_('home.why.advantage')}
+				</p>
+			</Row>
+			<Row>
+				<h2>{$_('home.how.title')}</h2>
+			</Row>
+			<Row>
+				<div class="list">
+					<OrderedList>
+						<ListItem>
+							{$_('home.how.step1')}
+						</ListItem>
+						<ListItem>
+							{$_('home.how.step2')}
+						</ListItem>
+						<ListItem>
+							{$_('home.how.step3')}
+						</ListItem>
+						<ListItem>
+							{$_('home.how.step4')}
+						</ListItem>
+					</OrderedList>
+				</div>
+			</Row>
+			<Row>
+				<h2>{$_('home.goal.title')}</h2>
+			</Row>
+			<Row>
+				<p>
+					{$_('home.goal.description')}
+				</p>
+			</Row>
+			<Row>
+				<h2>{$_('home.progress.title')}</h2>
+			</Row>
+			<Row>
+				<img src={demo} alt="demo" id="demo" />
+				<p>
+					{$_('home.progress.current')}
+				</p>
+			</Row>
+		</Grid>
+	</Content>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+	h2 {
+		margin-bottom: 1rem;
+		margin-top: 1rem;
 	}
 
-	h1 {
-		width: 100%;
+	p {
+		margin-bottom: 1rem;
+		margin-top: 1rem;
+	}
+
+	.list {
+		margin-left: 2rem;
+	}
+
+	#demo {
+		width: 80%;
+		max-width: 300px;
 	}
 </style>
